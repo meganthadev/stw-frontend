34baf4d7b84f81e58e1999542fc82b8645a19ba9
meganthadev <meganthadev@gmail.com>
2020-09-22 14:01:42 -0700

    Add Route, Link to product name

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom'
 
 const Products = (props) => {
 
     return (
         <div>
-          {props.products.map(product => <li key={product.id}>{product.name} --- Rating:  {product.rating}</li>) }
-        </div>
+        {props.products.map(product =>
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.name} </Link>
+          </li> )}
+      </div>
+  
     )
 
 }

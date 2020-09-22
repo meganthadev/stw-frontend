024a55b0d3a6501dd3004409639f63bdefb5b134
meganthadev <meganthadev@gmail.com>
2020-09-22 03:46:34 -0700

    Import/Add Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
 import productReducer from './reducers/productReducer';
 
 
@@ -15,9 +16,10 @@ let store = createStore(productReducer, composeEnhancers(applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     document.getElementById('root'));
 
-export default App;
 

7ab30399df6f0d21552b85c0919fa76301fc64f0
meganthadev <meganthadev@gmail.com>
2020-09-22 14:03:16 -0700

    Add addProduct case with spread op

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -3,6 +3,8 @@ export default function productReducer(state = {products: []}, action) {
     switch (action.type) {
         case 'FETCH_PRODUCTS':
             return {products: action.payload}
+        case 'ADD_PRODUCT':
+            return {...state, products: [...state.products, action.payload]}
     default:
         return state
     }

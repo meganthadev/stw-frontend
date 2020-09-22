7d40554919ba60a507a5d0e244ed02fd8b07131f
meganthadev <meganthadev@gmail.com>
2020-09-22 03:45:58 -0700

    Add Product Container to App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { fetchProducts } from './actions/fetchProducts';
+import ProductsContainer from './containers/ProductsContainer';
 
 class App extends React.Component {
-
-  componentDidMount() {
-    this.props.fetchProducts({type: 'FETCH_PRODUCTS', payload: {name: 'Avene'}})
-  }
 
     render() {
     return (
       <div className="App">
-        App
+        <ProductsContainer />
       </div>
     );
   }
 }
 
 
-export default connect(null, {fetchProducts})(App);
+export default App;

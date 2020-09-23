a9ebf2e58ff7d9b53e70bae3df4cbc727f6b542f
meganthadev <meganthadev@gmail.com>
2020-09-22 20:07:42 -0700

    Create JournalEntry Form

---
+++ src/components/JournalEntryForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class JournalEntryForm extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Journal Entry Form
+            </div>
+        )
+    }
+
+
+}
+
+
+export default JournalEntryForm;
+
+
+

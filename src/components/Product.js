4aa18e6e09089f7bf2f488a4b9106b2c16278c73
meganthadev <meganthadev@gmail.com>
2020-09-23 03:09:06 -0700

    Refactor

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import JournalEntriesContainer from '../containers/JournalEntriesContainer'
+import ProductEdit from './ProductEdit';
 
 const Product = (props) => {
 
@@ -9,9 +10,14 @@ const Product = (props) => {
     return (
         <div>
             <h2>
-                {product ? product.name : null}  --- Your Rating: {product ? product.rating : null}
+                {product ? product.name : null}  --- Your Current Rating: {product ? product.rating : null}
             </h2>
+            <p>
+                Image URL: {product ? product.image_url : null} </p> <p>Product Description: {product ? product.description : null}
+            </p><p>
+                Ingredients: {product ? product.ingredients : null} </p>
             <JournalEntriesContainer product={product} />
+            <h3>Change {product ? product.name : null}'s Rating:  </h3><ProductEdit product={product} />
         </div>
     )
 

349bcd2d875fbde4e6cfbaa642e97de366f4ad19
meganthadev <meganthadev@gmail.com>
2020-09-23 16:50:31 -0700

    Add onRateChange button to hide/show ProductEdit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,46 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import JournalEntriesContainer from '../containers/JournalEntriesContainer'
 import ProductEdit from './ProductEdit';
 
-const Product = (props) => {
+class Product extends React.Component {
 
-    let product = props.products.filter(product => product.id == props.match.params.id)[0]
+    state = {
+        viewProductEdit: false
+    }
 
-    return (
-        <div>
-            <h2>
-                {product ? product.name : null}  --- Your Current Rating: {product ? product.rating : null}
-            </h2>
-            <p>
-                Image URL: {product ? product.image_url : null} </p> <p>Product Description: {product ? product.description : null}
-            </p><p>
-                Ingredients: {product ? product.ingredients : null} </p>
-            <JournalEntriesContainer product={product} />
-            <h3>Change {product ? product.name : null}'s Rating:  </h3><ProductEdit product={product} />
-        </div>
-    )
+    onRateChange = () => {
+        this.setState(previousState => ({
+            viewProductEdit: previousState.viewProductEdit ? false : true
+        })
+       )
+    }
 
+
+    render() {
+
+        let product = this.props.products.filter(product => product.id == this.props.match.params.id)[0]
+
+        let changeRateButton = this.state.viewProductEdit ? "Close Rating Box" : "Update Product Rating"
+
+        return (
+            <div>
+                <h2>{product ? product.name : null}  </h2>
+                <h4>Your Current Rating: {product ? product.rating : null}</h4> 
+                <button class="button" onClick={this.onRateChange}>{changeRateButton}</button> 
+                { this.state.viewProductEdit  ?  <ProductEdit product={product} /> : null }
+
+                <p> {product ? <img src={product.image_url} /> : null} </p> 
+
+                <h4>Product Description: </h4>{product ? product.description : null}
+                <h4> Ingredients: </h4>{product ? product.ingredients : null} <br></br><br></br>
+                <JournalEntriesContainer product={product} />
+               
+            </div>
+        )
+    }
 }
 
-export default Product;
+
+    export default Product;

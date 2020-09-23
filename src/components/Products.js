5be93f7459141706084ab1db455805ca8d843102
meganthadev <meganthadev@gmail.com>
2020-09-22 17:20:15 -0700

    Add product var and add to return statement

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,18 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import {Route, Link} from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
+import Product from './Product';
 
 const Products = (props) => {
 
-    return (
-        <div>
-        {props.products.map(product =>
-          <li key={product.id}>
-            <Link to={`/products/${product.id}`}>{product.name} </Link>
-          </li> )}
-      </div>
-  
-    )
+  return (
+    <div>
+      {props.products.map(product =>
+        <div key={product.id}><Product product={product} /></div>
+      )}
+    </div>
+
+  )
 
 }
 

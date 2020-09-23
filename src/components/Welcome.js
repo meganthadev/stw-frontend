f5bcbccea571d69080b372f25a4acb9395812137
meganthadev <meganthadev@gmail.com>
2020-09-23 16:53:11 -0700

    Add text to welcome page, add story

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
-import Product from './Product';
 
 class Welcome extends React.Component {
 
   render() {
     return (
-    <div>
-        <Link to='/products/' >Check Out Your Products!</Link><br></br><br></br>
-        <Link to='/products/new' >Add a New Product!</Link>
-    </div>
+      <div>
+        <h3>Welcome to Skin That Wins!</h3>
+        <h4>What a wonderful world it is! Well... as wonderful as the world can be... it can actually hurt our skin sometimes. I know - shocker, right?!</h4><h5> We are all so different, we all have different skin types and life styles, and there are so many different skin care brands and lines and products and ingredients - and not to mention they all require your hard earned money! People all over the world have different concerns and conditions and sometimes it can feel like the skin care companies do not care - Enter Skin That Wins! An honest platform to share your skin care "Gos and Woes" (aka what works for you and what does not)! A wealth of information shared by a community of skincare (and haircare) afficionados, who are not looking to sell you products, but are just looking to share, and maybe gain, some new light and knowledge in the sea of skincare!</h5><br></br><br></br>
+      </div>
 
-  )
+    )
 
-}
+  }
 }
 
 export default Welcome;

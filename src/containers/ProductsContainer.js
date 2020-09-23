7fdf066762acb45fd98b775ffd8c7b337afbe17b
meganthadev <meganthadev@gmail.com>
2020-09-23 16:55:25 -0700

    Add exact welcome path link

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -5,7 +5,8 @@ import { fetchProducts } from '../actions/fetchProducts';
 import Product from '../components/Product';
 import ProductForm from '../components/ProductForm';
 import Products from '../components/Products';
-
+import NavBar from '../components/NavBar'
+import Welcome from '../components/Welcome'
 
 class ProductsContainer extends React.Component {
 
@@ -16,10 +17,12 @@ class ProductsContainer extends React.Component {
     render() {
         return (
             <div>
+                <NavBar />
                 <Switch>
                     <Route path='/products/new' component={ProductForm} />
                     <Route path='/products/:id' render={(routerProps) => <Product {...routerProps} products={this.props.products} />} />
                     <Route exact path='/products' render={(routerProps) => <Products {...routerProps} products={this.props.products} />} />
+                    <Route exact path='/' component={Welcome} />
                 </Switch>
             </div>
         )

10796a07e21e0eb53e6af1d22f075504e3a28069
meganthadev <meganthadev@gmail.com>
2020-09-22 17:20:53 -0700

    Add Route paths for new, prod show, products components

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Route} from 'react-router-dom';
 
+import Product from '../components/Product';
 import ProductForm from '../components/ProductForm';
 import Products from '../components/Products';
 import {fetchProducts} from '../actions/fetchProducts';
@@ -16,8 +18,10 @@ class ProductsContainer extends React.Component {
     render() {
         return (
             <div>
-                <ProductForm />
-                <Products products={this.props.products} />
+                <Route path='/products/new' component={ProductForm} /><br></br>
+                <Route path='/products/:id' render={(routerProps) => <Product {...routerProps} products={this.props.products} />} />
+                <Route exact path='/products' render={(routerProps) => <Products {...routerProps}  products={this.props.products} />} />
+
             </div>
         )
     }

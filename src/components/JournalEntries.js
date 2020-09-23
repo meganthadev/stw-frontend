ff4d8758414c7f8ebfa45d0e8fa1918737870e40
meganthadev <meganthadev@gmail.com>
2020-09-22 20:07:13 -0700

    Create JournalEntries component

---
+++ src/components/JournalEntries.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const JournalEntries = (props) => {
+
+    return(
+        <div>
+            Journal Entries
+        </div>
+    )
+
+}
+
+export default JournalEntries;

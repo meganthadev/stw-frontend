5fcc2bd0fb8cc48e065f501653a48ab7562422ca
meganthadev <meganthadev@gmail.com>
2020-09-23 16:47:35 -0700

    Import index.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ProductsContainer from './containers/ProductsContainer';
+import './index.css';
 
 class App extends React.Component {
 

7326714befca3560b47b1b7909941f0d6b0b29b3
meganthadev <meganthadev@gmail.com>
2020-09-23 03:07:21 -0700

    Reffactor

--- src/actions/deleteJournalEntry.js
+++ src/actions/deleteJournalEntry.js
@@ -4,8 +4,9 @@ export const deleteJournalEntry = (entryId, productId) => {
         fetch(`http://localhost:3000/api/v1/products/${productId}/journal_entries/${entryId}`, {
             method: 'DELETE'
         })
-        .then(resp => resp.json())
-        .then(product => dispatch({
-            type: 'DELETE_ENTRY', payload: product}))
+            .then(resp => resp.json())
+            .then(product => dispatch({
+                type: 'DELETE_ENTRY', payload: product
+            }))
     }
 }

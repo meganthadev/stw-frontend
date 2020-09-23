d17fa74506f98ad8c80ecb139503c247d425e0ab
meganthadev <meganthadev@gmail.com>
2020-09-23 03:06:29 -0700

    Refactor

--- src/containers/JournalEntriesContainer.js
+++ src/containers/JournalEntriesContainer.js
@@ -4,11 +4,14 @@ import JournalEntries from '../components/JournalEntries';
 
 class JournalEntriesContainer extends React.Component {
 
+
+
     render() {
         return (
             <div>
                 <JournalEntryForm product={this.props.product} />
-                <h3>Your Thoughts Over Time:</h3><JournalEntries journal_entries={this.props.product && this.props.product.journal_entries}/>
+                <h3>Your Thoughts On {this.props.product ? this.props.product.name : null} Over Time:</h3>
+                <JournalEntries journal_entries={this.props.product && this.props.product.journal_entries} />
             </div>
         )
     }

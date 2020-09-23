eff374f4a0f195f220a1b25c8847f14ed116834a
meganthadev <meganthadev@gmail.com>
2020-09-23 00:59:30 -0700

    Add delete entry dispatch action

---
+++ src/actions/deleteJournalEntry.js
@@ -0,0 +1,11 @@
+export const deleteJournalEntry = (entryId, productId) => {
+
+    return (dispatch) => {
+        fetch(`http://localhost:3000/api/v1/products/${productId}/journal_entries/${entryId}`, {
+            method: 'DELETE'
+        })
+        .then(resp => resp.json())
+        .then(product => dispatch({
+            type: 'DELETE_ENTRY', payload: product}))
+    }
+}

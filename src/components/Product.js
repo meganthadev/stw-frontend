9c85cd9a88ece7978febb958b5459f08311523b2
meganthadev <meganthadev@gmail.com>
2020-09-22 20:02:46 -0700

    Add Journal Entry Container to Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import JournalEntriesContainer from '../containers/JournalEntriesContainer'
 
 const Product = (props) => {
-console.log(props)
 
-let product = props.products[props.match.params.id - 1]
-console.log(product)
+    let product = props.products[props.match.params.id - 1]
+    console.log(product)
 
-    return(
-        <li>
-           { product ? product.name : null}  ---  Rating: { product ? product.rating : null}
-        </li>
+    return (
+        <div>
+            <h2>
+                {product ? product.name : null}  ---  Rating: {product ? product.rating : null}
+            </h2>
+            <JournalEntriesContainer product={product}/>
+        </div>
     )
 
 }

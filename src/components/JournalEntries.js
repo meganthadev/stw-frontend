ca1ac6e61a4003c6e07cfc64b05ba7e66e2997ff
meganthadev <meganthadev@gmail.com>
2020-09-23 00:57:58 -0700

    Add handleDelete() to jEntries Comp

--- src/components/JournalEntries.js
+++ src/components/JournalEntries.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Product from './Product';
+import {connect} from 'react-redux';
+import {deleteJournalEntry} from '../actions/deleteJournalEntry';
 
 const JournalEntries = (props) => {
+
+    const handleDelete = (journal_entry) => {
+        props.deleteJournalEntry(journal_entry.id, journal_entry.product_id)
+    }
 
     return(
         <div>
             {props.journal_entries && props.journal_entries.map(journal_entry => 
-              <li key={journal_entry.id}><strong>Your Skin was Feeling:  {journal_entry.entry_type} </strong> <h4>Your Skin was Saying... </h4>{journal_entry.entry}<br></br><br></br><br></br>
+              <li key={journal_entry.id}>   
+              <strong>Your Skin was Feeling:  {journal_entry.entry_type} </strong> 
+              <h4>And Your Skin was Telling You... </h4>{journal_entry.entry}<br></br><br></br><br></br>
+              <button onClick={() => handleDelete(journal_entry)}>Delete</button><br></br><br></br><br></br>
               </li> 
                 )}
         </div>
@@ -14,4 +23,4 @@ const JournalEntries = (props) => {
 
 }
 
-export default JournalEntries;
+export default connect(null, {deleteJournalEntry})(JournalEntries);

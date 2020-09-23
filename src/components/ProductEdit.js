58306a6f92ba219032ca9d931088fe79199c1c9b
meganthadev <meganthadev@gmail.com>
2020-09-23 16:51:12 -0700

    Change text content ProdEdit

--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -16,25 +16,26 @@ class ProductEdit extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-        let product = {...this.state, id: this.props.product.id}
+        let product = { ...this.state, id: this.props.product.id }
         this.props.editProduct(product)
         this.setState({
-             rating: ''
+            rating: ''
         })
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit} >                
+                <h3>Change Your Rating for {this.props.product ? this.props.product.name : null}: </h3>
+                <form onSubmit={this.handleSubmit} >
                     <label>Product Rating:  </label>
-                    <select type="number" name="rating" value={this.state.rating} onChange={this.handleChange} ><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
+                    <select class="button" type="number" name="rating" value={this.state.rating} onChange={this.handleChange} ><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option>
                     </select><br></br><br></br>
-                    <input type="submit" /><br></br><br></br>
+                    <input class="button" type="submit" /><br></br><br></br>
                 </form>
             </div>
         )
     }
 }
 
-    export default connect(null, { editProduct })(ProductEdit);
+export default connect(null, { editProduct })(ProductEdit);

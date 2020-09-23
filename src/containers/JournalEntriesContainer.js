6ed14d6e059599838a473e21c88458e9b746811c
meganthadev <meganthadev@gmail.com>
2020-09-23 16:55:00 -0700

    Add newEntryButton to hide/show jentry form

--- src/containers/JournalEntriesContainer.js
+++ src/containers/JournalEntriesContainer.js
@@ -4,13 +4,27 @@ import JournalEntries from '../components/JournalEntries';
 
 class JournalEntriesContainer extends React.Component {
 
+    state = {
+        viewNewEntry: false
+    }
 
+    onNewEntry = () => {
+        this.setState(previousState => ({
+            viewNewEntry: previousState.viewNewEntry ? false : true
+        })
+        )
+    }
 
     render() {
+
+        let newEntryButton = this.state.viewNewEntry ? "Close Journal Box" : "Dish About This Product"
+
         return (
             <div>
-                <JournalEntryForm product={this.props.product} />
-                <h3>Your Thoughts On {this.props.product ? this.props.product.name : null} Over Time:</h3>
+                <button class="button" onClick={this.onNewEntry}>{newEntryButton}</button>
+                { this.state.viewNewEntry ? <JournalEntryForm product={this.props.product} /> : null}
+
+                <h3>Your Previous Thoughts On {this.props.product ? this.props.product.name : null}:</h3>
                 <JournalEntries journal_entries={this.props.product && this.props.product.journal_entries} />
             </div>
         )

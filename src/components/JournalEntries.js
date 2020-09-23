9ffbbf6b66ed0b95d2d7746c150e4ea28347ef0b
meganthadev <meganthadev@gmail.com>
2020-09-22 23:41:11 -0700

    Refactor/format journal entries show

--- src/components/JournalEntries.js
+++ src/components/JournalEntries.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Product from './Product';
 
 const JournalEntries = (props) => {
 
     return(
         <div>
-            Journal Entries
+            {props.journal_entries && props.journal_entries.map(journal_entry => 
+              <li key={journal_entry.id}><strong>Your Skin was Feeling:  {journal_entry.entry_type} </strong> <h4>Your Skin was Saying... </h4>{journal_entry.entry}<br></br><br></br><br></br>
+              </li> 
+                )}
         </div>
     )
 

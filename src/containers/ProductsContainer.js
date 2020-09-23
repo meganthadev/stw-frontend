cbba475b34e65262639fb1dae043c748c62fe03c
meganthadev <meganthadev@gmail.com>
2020-09-22 20:04:26 -0700

    Add Switch statement to Prod Container

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Route} from 'react-router-dom';
-
+import { Route, Switch } from 'react-router-dom';
+import { fetchProducts } from '../actions/fetchProducts';
 import Product from '../components/Product';
 import ProductForm from '../components/ProductForm';
 import Products from '../components/Products';
-import {fetchProducts} from '../actions/fetchProducts';
-
 
 
 class ProductsContainer extends React.Component {
@@ -18,10 +16,11 @@ class ProductsContainer extends React.Component {
     render() {
         return (
             <div>
-                <Route path='/products/new' component={ProductForm} /><br></br>
-                <Route path='/products/:id' render={(routerProps) => <Product {...routerProps} products={this.props.products} />} />
-                <Route exact path='/products' render={(routerProps) => <Products {...routerProps}  products={this.props.products} />} />
-
+                <Switch>
+                    <Route path='/products/new' component={ProductForm} />
+                    <Route path='/products/:id' render={(routerProps) => <Product {...routerProps} products={this.props.products} />} />
+                    <Route exact path='/products' render={(routerProps) => <Products {...routerProps} products={this.props.products} />} />
+                </Switch>
             </div>
         )
     }
@@ -30,9 +29,9 @@ class ProductsContainer extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-       products: state.products
+        products: state.products
     }
 
 }
 
-export default connect(mapStateToProps, {fetchProducts})(ProductsContainer);
+export default connect(mapStateToProps, { fetchProducts })(ProductsContainer);
